6bab5d8dac8cbcffdc1b18074cd691f19a073ee3
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-11-29 12:35:48 +0100

    index page edit

--- index.js
+++ index.js
@@ -36,11 +36,15 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// })
+
+app.get("/", (req, res) => {
+  res.send("node real estate backend 1.0.0");
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
